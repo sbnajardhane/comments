045fefd13f69a75c371d531ca28740ee00320899
root <root@shubham-Notebook-14.(none)>
2017-05-02 23:23:39 +0530

    changes in index html to add the selection

--- comments.js
+++ comments.js
@@ -10,27 +10,31 @@ function getSelectedText() {
 var a;
 var evn = "shubham";
 var count = 0;
-$('#commands p').mouseup(function(e) {
+$('#description span').mouseup(function(e) {
     var selectionObject = getSelectedText();
     var text = selectionObject.toString();
     if (text != '') {
         console.log("text == " + text);
         evn = e;
         a = selectionObject;
-        console.log(selectionObject);
+        // console.log(selectionObject);
         // console.log(event.currentTarget.innerHTML);
         // console.log(event);
         a = selectionObject;
-        var startIndecx = selectionObject.anchorOffset;
-        var endIndex = selectionObject.focusOffset;
+        var startIndecx = selectionObject.anchorOffset < selectionObject.focusOffset ? selectionObject.anchorOffset : selectionObject.focusOffset;
+        var endIndex = selectionObject.focusOffset > selectionObject.anchorOffset ? selectionObject.focusOffset : selectionObject.anchorOffset;
         var description = selectionObject.focusNode.data;
-        console.log(selectionObject.focusNode.outerHTML);
+        console.log(selectionObject.anchorNode);
         var array = [];
         array[0] = description.slice(0, startIndecx);
         array[1] = description.slice(endIndex);
         console.log(startIndecx + " endIndex" + endIndex);
-        console.log(array);
+        // console.log(array);
         count++;
-        e.target.innerHTML = array[0] + '<span id="' + count + '">' + text + '</span>' + array[1];
+        var remaining = e.target.innerHTML;
+        console.log(remaining);
+        var remainingLength = selectionObject.anchorNode.length;
+        remaining = remaining.slice(0, -remainingLength);
+        e.target.innerHTML = remaining + array[0] + '<span class="green" id="' + count + '">' + text + '</span>' + array[1];
     }
 });
