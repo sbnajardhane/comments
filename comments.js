a530e14dbf3ae541eeae67aa45aae189e2ace77a
shubham <sbnajardhane@gmail.com>
2017-05-03 21:47:56 +0530

    created a jquery module object approach

--- comments.js
+++ comments.js
@@ -12,80 +12,56 @@ var evn = "shubham";
 var anchorNode;
 $(function() {
     var comments = (function() {
-            var selectionObject = getSelectedText();
-            var text = selectionObject.toString();
-            var count = 0;
-            $('#description p').mouseup(function(e) {});
-            var selectText = function() {
+        var selectionObject; // = getSelectedText();
+        var text; // = selectionObject.toString();
+        var count = 0;
+        //bind event on mouse up
+        //function to select the text from the html desc
+        //select and open dialog box
+
+        var __createNode = function() {
+            //create span for the selected text and changes the color
+            var highlightNode = document.createElement("span");
+            highlightNode.setAttribute("class", "green");
+            highlightNode.setAttribute("id", count);
+            // append it to HTML
+            console.log(selectionObject);
+            var range = selectionObject.getRangeAt(0);
+            range.surroundContents(highlightNode);
+
+            //append created comment element after highlighted span
+            // $(#count).after(commentNode);
+
+
+            count++;
+        };
+
+        var selectText = function() {
+            selectionObject = getSelectedText();
+            text = selectionObject.toString();
+            range = selectionObject.getRangeAt(0);
+            console.log(text);
+            if (text != '') {
+                var dialog = $("#dialog");
                 console.log(text);
-                if (text != '') {
-
-                    //open modal
-                    //
-
-                    var dialog = $("#dialog");
-                    // dialog[0].setAttribute("title", text);
-                    // $("#dialog").dialog('title', text);
-                    dialog.dialog();
-                }
+                // dialog[0].setAttribute("title", text);
+                // $("#dialog").dialog('title', text);
+                dialog.dialog();
             }
+        };
 
-            //
-
-            //create comment element p
-            //
-
-            //click on add comment button
-            //
+        //create span element
 
 
-            var createNode = function() {
-                //create span for the selected text and changes the color
-                evn = e;
-                a = selectionObject;
-                var highlightNode = document.createElement("span");
-                highlightNode.setAttribute("class", "green");
-                highlightNode.setAttribute("id", count);
-
-                // append it to HTML
-                var range = selectionObject.getRangeAt(0);
-                range.surroundContents(highlightNode);
-
-                //append created comment element after highlighted span
-                // $(#count).after(commentNode);
-
-
-                count++;
-            }
-        }
-
-
-        $("#description").click(function(event) {
-            target = event.target;
-
-            // console.log(event.target.nodeName);
-            if (target.nodeName === "SPAN") {
-                var id = target.attributes.id.value;
-                console.log(target.attributes.id);
-                // id = parseInt(id);
-                // id = id + id;
-
-                //create comment box
-
-                console.log(target);
-                target.after(commentNode);
-
-            }
-
-        });
-
-        $('#dialogComment').keyup(function() {
+        var disabledButton = function() {
             if ($(this).val().length != 0) {
                 $('#addComment').attr('disabled', false);
             } else {
                 $('#addComment').attr('disabled', true);
             }
-        }) $('#addComment').on('click', function() {
+        };
+
+        var addComment = function() {
             console.log('click event');
             var commentNode = document.createElement("p");
             commentNode.innerText = $("#dialogComment").val(); //"this is comment regarding to the id " + count; //get the content here from input box
@@ -93,8 +69,31 @@ $(function() {
             var comments = document.getElementById("comments");
             comments.appendChild(commentNode);
             $("#dialog").dialog('close');
-            // displayComment(id);
-            // createNode();
-        });
-    });
+            __createNode();
+        };
+
+        var viewComment = function() {
+            target = event.target;
+            if (target.nodeName === "SPAN") {
+                var id = target.attributes.id.value;
+                target.after(commentNode);
+            }
+        };
+
+        var bindFunctions = function() {
+            $('#description p').mouseup(selectText);
+            $('#dialogComment').on('keyup', disabledButton);
+            $('#addComment').on('click', addComment);
+            $("#description").click(function(event) {});
+        };
+
+        var init = function() {
+            bindFunctions();
+        };
+
+        return {
+            init: init,
+        };
+    })();
+    comments.init();
 });
