fcd36b7993edceb68f3facaf04c0f633acf0ffbf
shubham <sbnajardhane@gmail.com>
2017-05-03 19:02:09 +0530

    unstable changes to handle the jquery model plugin

--- comments.js
+++ comments.js
@@ -8,83 +8,93 @@ function getSelectedText() {
 }
 var a;
 var evn = "shubham";
-var count = 0;
+
 var anchorNode;
 $(function() {
-    $('#description p').mouseup(function(e) {
-        var selectionObject = getSelectedText();
-        var text = selectionObject.toString();
-        console.log(text);
-        if (text != '') {
+    var comments = (function() {
+            var selectionObject = getSelectedText();
+            var text = selectionObject.toString();
+            var count = 0;
+            $('#description p').mouseup(function(e) {});
+            var selectText = function() {
+                console.log(text);
+                if (text != '') {
 
-            //open modal
-            //
+                    //open modal
+                    //
 
-            var dialog = $("#dialog");
-
-            dialog[0].setAttribute("title", text);
-            dialog.dialog();
-            dialog[0].setAttribute("title", "");
+                    var dialog = $("#dialog");
+                    // dialog[0].setAttribute("title", text);
+                    // $("#dialog").dialog('title', text);
+                    dialog.dialog();
+                }
+            }
 
             //
 
             //create comment element p
             //
-            $('#dialogComment').on('click', function() {
-                var commentNode = document.createElement("p");
-                commentNode.innerText = $("#dialogComment").val(); //"this is comment regarding to the id " + count; //get the content here from input box
-                commentNode.setAttribute("class", "hidden");
-                commentNode.setAttribute("id", count + "desc");
-            });
+
             //click on add comment button
             //
 
 
+            var createNode = function() {
+                //create span for the selected text and changes the color
+                evn = e;
+                a = selectionObject;
+                var highlightNode = document.createElement("span");
+                highlightNode.setAttribute("class", "green");
+                highlightNode.setAttribute("id", count);
 
-            //create span for the selected text and changes the color
-            evn = e;
-            a = selectionObject;
-            var highlightNode = document.createElement("span");
-            highlightNode.setAttribute("class", "green");
-            highlightNode.setAttribute("id", count);
+                // append it to HTML
+                var range = selectionObject.getRangeAt(0);
+                range.surroundContents(highlightNode);
 
-            // append it to HTML
-            var range = selectionObject.getRangeAt(0);
-            range.surroundContents(highlightNode);
-
-            //append created comment element after highlighted span
-            // $(#count).after(commentNode);
+                //append created comment element after highlighted span
+                // $(#count).after(commentNode);
 
 
-            count++;
+                count++;
+            }
         }
+
+
+        $("#description").click(function(event) {
+            target = event.target;
+
+            // console.log(event.target.nodeName);
+            if (target.nodeName === "SPAN") {
+                var id = target.attributes.id.value;
+                console.log(target.attributes.id);
+                // id = parseInt(id);
+                // id = id + id;
+
+                //create comment box
+
+                console.log(target);
+                target.after(commentNode);
+
+            }
+
+        });
+
+        $('#dialogComment').keyup(function() {
+            if ($(this).val().length != 0) {
+                $('#addComment').attr('disabled', false);
+            } else {
+                $('#addComment').attr('disabled', true);
+            }
+        }) $('#addComment').on('click', function() {
+            console.log('click event');
+            var commentNode = document.createElement("p");
+            commentNode.innerText = $("#dialogComment").val(); //"this is comment regarding to the id " + count; //get the content here from input box
+            commentNode.setAttribute("id", "comment_" + count);
+            var comments = document.getElementById("comments");
+            comments.appendChild(commentNode);
+            $("#dialog").dialog('close');
+            // displayComment(id);
+            // createNode();
+        });
     });
-
-    $("#description").click(function(event) {
-        target = event.target;
-
-        // console.log(event.target.nodeName);
-        if (target.nodeName === "SPAN") {
-            var id = target.attributes.id.value;
-            console.log(target.attributes.id);
-            // id = parseInt(id);
-            // id = id + id;
-
-            //create comment box
-
-            console.log(target);
-            target.after(commentNode);
-
-        }
-
-    });
-
-    $('#dialogComment').keyup(function() {
-        if ($(this).val().length != 0) {
-            $('#addComment').attr('disabled', false);
-        } else {
-            $('#addComment').attr('disabled', true);
-        }
-    })
-
 });
